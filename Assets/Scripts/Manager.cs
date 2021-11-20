850b7457e40ddfa859480e0d9c68fe7cc04f4603
Alex Jhones <alexjhones286@gmail.com>
2021-11-20 18:48:08 -0300

    Adicionando Funções Bancárias

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,10 +4,51 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    public int accountNumber;
+    public string name, type;
+    private float money;
+    private bool status;
+
+    public void SetAccountNumber(int value) {
+        accountNumber = value;
+    }
+
+    /*public GetAccountNumber() {
+        return accountNumber;
+    }*/
+
+    /*
+    public OpenAccount() {
+
+
+    }
+
+    public CloseAccount() {
+
+
+    }
+
+    public Deposit() {
+
+
+    }
+
+    public Withdraw() {
+
+
+    }
+
+    public Pay() {
+
+    }
+
+    */
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        money = 0;
+        print(money);
     }
 
     // Update is called once per frame
